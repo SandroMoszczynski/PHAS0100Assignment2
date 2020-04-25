c278c84d69694e3248de668ba973391b583eb35b
sukrire <sandromarcus@hotmail.co.uk>
2020-04-25 14:57:31 +0100

    benchmarking done, may export to text file in  master branch

--- Code/CommandLineApps/sfmOpen_MP.cpp
+++ Code/CommandLineApps/sfmOpen_MP.cpp
@@ -18,14 +18,14 @@
 std::vector<std::shared_ptr<sfm::Forces> > &update_pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians)
 {
     double dt = 0.1; //s
-    double finish_time_s = 10;//second
+    double finish_time_s = 50;//second
     double v_max = 1.3;
     sfm::dir2d temp_force;
     sfm::dir2d new_velocity;
     sfm::dir2d position;
     sfm::pos2d new_position;
     for(int t=0; t<(finish_time_s/dt);++t){
-        #pragma omp paralell private(new_velocity),private(position),private(new_position), firstprivate(temp_force), shared(pedestrians)
+        #pragma omp tasks private(new_velocity),private(position),private(new_position), firstprivate(temp_force), shared(pedestrians)
         {
             #pragma omp for
             for(int j=0; j<pedestrians.size();++j){               
@@ -47,9 +47,8 @@ std::vector<std::shared_ptr<sfm::Forces> > &update_pedestrian(std::vector<std::s
 
 std::vector<std::shared_ptr<sfm::Forces> > &Create_Pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians)
 {
-    int no_pedestrians = 500;
+    int no_pedestrians = 1000;
     //std::cin >> no_pedestrians;
-    
     sfm::dir2d left_side_x(1,1);
     sfm::dir2d left_side_y(0.1,9.9);
     sfm::dir2d dest_left_x(48,49);
@@ -76,196 +75,55 @@ std::vector<std::shared_ptr<sfm::Forces> > &Create_Pedestrian(std::vector<std::s
 
 int main()
 {
-    #ifdef _OPENMP
-    omp_set_dynamic(0);
-    //omp_set_num_threads(14);
-    #endif
+    std::cout   << "Testing done with 1000 pedestrians \n"
+                << "For time 100 seconds at intervals of 0.1s\n" << std::endl;
 
-    std::vector<std::shared_ptr<sfm::Forces> >pedestrians;
+    // done without multi threading 
+    std::vector<std::shared_ptr<sfm::Forces> >pedestrians1;
+    std::cout << "start no mp" << std::endl;
+    std::clock_t c_start1 = std::clock();
+    auto t_start1 = std::chrono::high_resolution_clock::now();
+    pedestrians1 = Create_Pedestrian(pedestrians1);
+    pedestrians1 = update_pedestrian(pedestrians1);
+    std::clock_t c_end1 = std::clock();
+    auto t_end1 = std::chrono::high_resolution_clock::now();
+    std::cout << "stop no mp" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used no mp: "
+            << 1000.0 * (c_end1-c_start1) / CLOCKS_PER_SEC << " ms\n"
+            << "Wall clock time passed no mp: "
+            << std::chrono::duration<double, std::milli>(t_end1-t_start1).count()
+            << " ms\n";
 
-
-    std::cout << "start" << std::endl;
-    std::clock_t c_start = std::clock();
-    auto t_start = std::chrono::high_resolution_clock::now();
-    pedestrians = Create_Pedestrian(pedestrians);
-    #pragma omp parallel shared(pedestrians)
+    //now iterate with incresing number of threads, to 28, max number for my pc
+    for(int num_threads = 1;num_threads<29;++num_threads)
     {
-        //#pragma single nowait
-        //{
-            pedestrians = update_pedestrian(pedestrians);
-        //}
+        std::cout << "Threadcount: " << num_threads << std::endl;
+        #ifdef _OPENMP
+        omp_set_num_threads(num_threads);
+        #endif
+
+        std::vector<std::shared_ptr<sfm::Forces> >pedestrians;
+
+
+        std::cout << "start with mp" << std::endl;
+        std::clock_t c_start = std::clock();
+        auto t_start = std::chrono::high_resolution_clock::now();
+        pedestrians = Create_Pedestrian(pedestrians);
+        #pragma omp parallel shared(pedestrians)
+        {
+            #pragma single nowait
+            {
+                pedestrians = update_pedestrian(pedestrians);
+            }
+        }
+        std::clock_t c_end = std::clock();
+        auto t_end = std::chrono::high_resolution_clock::now();
+        std::cout << "stop with mp" << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+                << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n"
+                << "Wall clock time passed: "
+                << std::chrono::duration<double, std::milli>(t_end-t_start).count()
+                << " ms\n";
     }
-    std::clock_t c_end = std::clock();
-    auto t_end = std::chrono::high_resolution_clock::now();
-    std::cout << "stop" << std::endl;
-    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
-              << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n"
-              << "Wall clock time passed: "
-              << std::chrono::duration<double, std::milli>(t_end-t_start).count()
-              << " ms\n";
     return 0;
 }
-
-
-
-
-
-// sfm::pos2d origin1(19,2); //b/w 50 and 10
-// sfm::pos2d destination1(16.285,2.89);
-// sfm::dir2d velocity1(1.65,0.3354);
-// sfm::pos2d current_position1(46,6);
-// double speed1 = 1.15;
-// double rest_time1 = 1;
-// sfm::Factory test1(origin1, destination1,velocity1, current_position1, speed1, rest_time1);
-// std::shared_ptr<sfm::Factory> sp1(new sfm::Factory(test1));
-// pedestrians.emplace_back(sp1);
-// pedestrians[0]->   
-
-// double x_min = 2;
-// double x_max = 2;
-// double y_min = 5;
-// double y_max = 5;
-
-// sfm::dir2d testa = sfm::Factory::R_Dir2d(testa,x_min,x_max,y_min,y_max);
-// sfm::pos2d testb = sfm::Factory::R_Pos2d(testb,x_min,x_max,y_min,y_max);
-// double testc = sfm::Factory::R_Doub(testc,1,1);
-
-// std::cout << testa[1] << ", " << testa[0] << " testa" << std::endl;
-// std::cout << testb[1] << ", " << testb[0] << " testb" << std::endl;
-// std::cout << testc << " testc" << std::endl;
-
-// int no_pedestrians = 5;
-// std::vector<std::shared_ptr<sfm::Forces> >pedestrians;
-// //pedestrians = sfm::Factory::Spawner(pedestrians,20); 
-// pedestrians = sfm::Factory::Spawner(pedestrians, no_pedestrians,
-//     0.1,
-//     1.5,
-//     0.1,
-//     1.5,
-//     0.1,
-//     1.5,
-//     0.1, 
-//     0.5, 
-//     0.1, 
-//     9.9,
-//     48, 
-//     49, 
-//     0.1, 
-//     9.9, 
-//     0.1,
-//     1.5
-// );
-//sfm::pos2d left_side_min = {0.1,0.1};
-//sfm::pos2d left_side_max = {0.5,9.9};
-// sfm::dir2d dest_final = {49,45};
-// std::cout << dest_final[1] << dest_final[0] <<std::endl;
-//pedestrians = sfm::Factory::Distributed(pedestrians,"Targeted",no_pedestrians,dest_final, left_side_min,left_side_max); 
-//pedestrians = sfm::Factory::Targeted(pedestrians,no_pedestrians,dest_final);
-
-// for(int i = 0;i<pedestrians.size();++i){
-//     std::cout << pedestrians[i]->Return_Current_Position()[1]   << " ," << pedestrians[i]->Return_Current_Position()[0]     << " current pos pedes:" << i << std::endl;
-//     std::cout << pedestrians[i]->Return_Origin()[1]             << " ," << pedestrians[i]->Return_Origin()[0]               << " origin pedes:" << i  << std::endl;
-//     std::cout << pedestrians[i]->Return_Velocity()[1]           << " ," << pedestrians[i]->Return_Velocity()[0]             << " velocity pedes:" << i  << std::endl;
-//     std::cout << pedestrians[i]->Return_Destination()[1]        << " ," << pedestrians[i]->Return_Destination()[0]          << " destination pedes:" << i  << std::endl;
-//     std::cout << pedestrians[i]->Return_Speed()                 << " ," << pedestrians[i]->Return_Rest_time()               << " speed,rest time pedes:" << i  << std::endl;
-// }
-
-// sfm::dir2d xy = {1,2};
-// std::cout << xy[0] << ", " << xy[1] << std::endl; 
-// sfm::dir2d xy2(xy[1],xy[0]); // WHYY?????
-// std::cout << xy2[0] << ", " << xy2[1] << std::endl;
-// std::default_random_engine generator;
-// std::vector<std::shared_ptr<sfm::Forces> >pedestrians;
-// for(int i=0;i<20;++i){
-//     std::uniform_real_distribution<double> origin_gen_x(0,50);  
-//     std::uniform_real_distribution<double> origin_gen_y(0,10);   
-//     std::uniform_real_distribution<double> destination_gen_x(0,50);  
-//     std::uniform_real_distribution<double> destination_gen_y(0,10);  
-//     std::uniform_real_distribution<double> velicty_gen(0,1);
-//     std::uniform_real_distribution<double> current_position_gen(0,10);  
-//     std::uniform_real_distribution<double> speed_gen(0.1,2);   
-//     std::uniform_real_distribution<double> resttime_gen(0.1,2);  
-//     sfm::pos2d origin = {origin_gen_x(generator),origin_gen_y(generator)}; //b/w 50 and 10
-//     sfm::pos2d destination = {destination_gen_x(generator), destination_gen_y(generator)};
-//     sfm::dir2d velocity = {velicty_gen(generator), velicty_gen(generator)};
-//     sfm::pos2d current_position = {current_position_gen(generator), current_position_gen(generator)};
-//     double speed = speed_gen(generator);
-//     double rest_time = resttime_gen(generator);
-//     sfm::Forces ped_spawn(origin,destination,velocity,current_position,speed,rest_time);
-//     std::shared_ptr<sfm::Forces> pointer(new sfm::Forces(ped_spawn));
-//     pedestrians.emplace_back(pointer);
-//}
-//sfm::dir2d totalforce = pedestrians[0]->Resultant_force(pedestrians,totalforce);
-//std::cout << totalforce[1] << std::endl;
-//std::cout << totalforce[0] << std::endl;
-//double speed = pedestrians[12]->Return_Speed();
-//std::cout << speed << std::endl;
-
-
-
-// sfm::pos2d origin1(19,2); //b/w 50 and 10
-// sfm::pos2d destination1(16.285,2.89);
-// sfm::dir2d velocity1(1.65,0.3354);
-// sfm::pos2d current_position1(46,6);
-// double speed1 = 1.15;
-// double rest_time1 = 1;
-// sfm::pos2d origin2(1,6); //b/w 50 and 10
-// sfm::pos2d destination2(46.50,1.648);
-// sfm::dir2d velocity2(1.56,0.354);
-// sfm::pos2d current_position2(15,5);
-// double speed2 = 1.55;
-// double rest_time2 = 0.5;
-// sfm::pos2d origin3(4,5); //b/w 50 and 10
-// sfm::pos2d destination3(23,9);
-// sfm::dir2d velocity3(1.8,0.3354);
-// sfm::pos2d current_position3(4,3);
-// double speed3 = 1.3;
-// double rest_time3 = 0.4;
-//sfm::Pedestrian test1(origin1, destination1,velocity1, current_position1, speed1, rest_time1);
-// sfm::Forces test1(origin1, destination1,velocity1, current_position1, speed1, rest_time1);
-// sfm::Forces test2(origin2, destination2,velocity2, current_position2, speed2, rest_time2);
-// sfm::Forces test3(origin3, destination3,velocity3, current_position3, speed3, rest_time3);
-//now i create all the tests variables
-// sfm::dir2d desdirec;
-// sfm::dir2d repforce;
-// sfm::dir2d ataforce;
-// sfm::dir2d ataforce2;
-// std::shared_ptr<sfm::Forces> sp1(new sfm::Forces(test1));
-// std::shared_ptr<sfm::Forces> sp2(new sfm::Forces(test2));
-// std::shared_ptr<sfm::Forces> sp3(new sfm::Forces(test3));
-// double elipse_test;
-// //elipse_test = test1.elipse(sp2,elipse_test);
-// //std::shared_ptr<sfm::Pedestrian> sp3(new sfm::Pedestrian(test3));
-// std::vector<std::shared_ptr<sfm::Forces> >pedestrians;
-// pedestrians.emplace_back(sp1);
-// pedestrians.emplace_back(sp2);
-// pedestrians.emplace_back(sp3);
-// //std::cout << pedestrians.size() << std::endl;
-// //repforce = sp1->border_repulsive(repforce);
-// //sfm::dir2d repforce1 = pedestrians[0]->border_repulsive(repforce1);
-// //sfm::dir2d repforce2 = test1.repulsive_force(pedestrians[2],repforce2);
-// //sfm::dir2d repforce3 = pedestrians[2]->border_repulsive(repforce3);
-// //desdirec = test1.desired_direction(desdirec);
-// //ataforce = test2.attractive_force(ataforce);
-// //repforce = test1.repulsive_force(pedestrians[1],repforce);
-// sfm::dir2d totalforce = test2.Resultant_force(pedestrians,totalforce);
-// //std::cout << sp1.get() << std::endl;
-// //std::cout << sp2.get() << std::endl;
-// //std::cout << sp3.get() << std::endl;
-// double test_speed;
-// sfm::pos2d test_origin;
-// sfm::pos2d test_destination;
-// sfm::dir2d test_velocity;
-// sfm::pos2d test_current_position(0,0);
-// //test_speed = force1.Return_Speed();
-// //test_speed = sp1->Return_Speed();
-// //std::cout << test_speed << std::endl;
-// //std::cout << elipse_test << std::endl;
-// std::cout << totalforce[1] << std::endl;
-// std::cout << totalforce[0] << std::endl;
-// //std::cout << repforce2[1] << std::endl;
-// //std::cout << repforce2[0] << std::endl;
-// // std::cout << repforce3[1] << std::endl;
-// // std::cout << repforce3[0] << std::endl;
-// //std::cout << ataforce2[1] << std::endl;
-// //std::cout << ataforce2[0] << std::endl;
