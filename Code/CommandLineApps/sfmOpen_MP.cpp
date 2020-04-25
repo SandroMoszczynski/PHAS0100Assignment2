fbf8a728450d08bd90901b68e1aeb23510d9c054
sukrire <sandromarcus@hotmail.co.uk>
2020-04-25 16:38:14 +0100

    better benchmarking #9

--- Code/CommandLineApps/sfmOpen_MP.cpp
+++ Code/CommandLineApps/sfmOpen_MP.cpp
@@ -11,15 +11,14 @@
 #include <chrono>
 #include <ctime>
 #include <fstream>
+#include <string>
 
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
-std::vector<std::shared_ptr<sfm::Forces> > &update_pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians)
+std::vector<std::shared_ptr<sfm::Forces> > &Update_Pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians, double dt, double finish_time_s)
 {
-    double dt = 0.1; //s
-    double finish_time_s = 50;//second
     double v_max = 1.3;
     sfm::dir2d temp_force;
     sfm::dir2d new_velocity;
@@ -46,10 +45,8 @@ std::vector<std::shared_ptr<sfm::Forces> > &update_pedestrian(std::vector<std::s
     return pedestrians;
 }
 
-std::vector<std::shared_ptr<sfm::Forces> > &Create_Pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians)
+std::vector<std::shared_ptr<sfm::Forces> > &Create_Pedestrian(std::vector<std::shared_ptr<sfm::Forces> > &pedestrians, int no_pedestrians)
 {
-    int no_pedestrians = 1000;
-    //std::cin >> no_pedestrians;
     sfm::dir2d left_side_x(1,1);
     sfm::dir2d left_side_y(0.1,9.9);
     sfm::dir2d dest_left_x(48,49);
@@ -76,20 +73,37 @@ std::vector<std::shared_ptr<sfm::Forces> > &Create_Pedestrian(std::vector<std::s
 
 int main()
 {
+    std::string choice;
+    std::cout << "Default?" << std::endl;
+    std::cin >> choice;
+    double dt = 0.1; //s
+    double finish_time_s;//second
+    int no_pedestrians;
+    if(choice ==  "Yes"){
+        finish_time_s = 100;//second
+        no_pedestrians = 1000;
+    }
+    else{
+        std::cout << "Finish Time/0.1?" << std::endl;
+        std::cin >> finish_time_s;
+        std::cout << "No Pedestrians?" << std::endl;
+        std::cin >> no_pedestrians;
+    }
+    
     std::ofstream Results;
     Results.open("Benchmarking.txt");
-    Results << "Testing done with 1000 pedestrians \n"
-            << "For time 100 seconds at intervals of 0.1s\n";
-    std::cout   << "Testing done with 1000 pedestrians \n"
-                << "For time 100 seconds at intervals of 0.1s\n" << std::endl;
+    Results << "Testing done with " << no_pedestrians << " pedestrians  \n"
+            << "For time " << finish_time_s << " seconds at intervals of 0.1s\n";
+    std::cout   << "Testing done with " << no_pedestrians << " pedestrians \n"
+                << "For time " << finish_time_s << " seconds at intervals of 0.1s\n" << std::endl;
 
     // done without multi threading 
     std::vector<std::shared_ptr<sfm::Forces> >pedestrians1;
     std::cout << "start no mp" << std::endl;
     std::clock_t c_start1 = std::clock();
     auto t_start1 = std::chrono::high_resolution_clock::now();
-    pedestrians1 = Create_Pedestrian(pedestrians1);
-    pedestrians1 = update_pedestrian(pedestrians1);
+    pedestrians1 = Create_Pedestrian(pedestrians1,no_pedestrians);
+    pedestrians1 = Update_Pedestrian(pedestrians1,dt,finish_time_s);
     std::clock_t c_end1 = std::clock();
     auto t_end1 = std::chrono::high_resolution_clock::now();
     std::cout << "stop no mp" << std::endl;
@@ -98,17 +112,19 @@ int main()
             << "Wall clock time passed no mp: "
             << std::chrono::duration<double, std::milli>(t_end1-t_start1).count()
             << " ms\n";
-    Results << std::fixed << std::setprecision(2) << "CPU time used no mp: "
-            << 1000.0 * (c_end1-c_start1) / CLOCKS_PER_SEC << " ms\n"
-            << "Wall clock time passed no mp: "
-            << std::chrono::duration<double, std::milli>(t_end1-t_start1).count()
-            << " ms\n";
+    Results << "Thread Count;   CPU time(ms):      Wall clock time(ms): \n"
+            << std::fixed << std::setprecision(2)
+            << "1, "
+            << 1000.0 * (c_end1-c_start1) / CLOCKS_PER_SEC << " ," 
+            << std::chrono::duration<double, std::milli>(t_end1-t_start1).count() <<"\n";
 
     //now iterate with incresing number of threads, to 28, max number for my pc
-    for(int num_threads = 1;num_threads<29;++num_threads)
+
+    int max_threads = omp_get_max_threads(); // throws an error but works
+
+    for(int num_threads = 1;num_threads<(max_threads+1);++num_threads)
     {
         std::cout << "Threadcount: " << num_threads << std::endl;
-        Results << "Threadcount: " << num_threads << " \n";
         #ifdef _OPENMP
         omp_set_num_threads(num_threads);
         #endif
@@ -119,12 +135,12 @@ int main()
         std::cout << "start with mp" << std::endl;
         std::clock_t c_start = std::clock();
         auto t_start = std::chrono::high_resolution_clock::now();
-        pedestrians = Create_Pedestrian(pedestrians);
+        pedestrians = Create_Pedestrian(pedestrians,no_pedestrians);
         #pragma omp parallel shared(pedestrians)
         {
             #pragma single nowait
             {
-                pedestrians = update_pedestrian(pedestrians);
+                pedestrians = Update_Pedestrian(pedestrians,dt,finish_time_s);
             }
         }
         std::clock_t c_end = std::clock();
@@ -135,11 +151,10 @@ int main()
                 << "Wall clock time passed: "
                 << std::chrono::duration<double, std::milli>(t_end-t_start).count()
                 << " ms\n";
-        Results << std::fixed << std::setprecision(2) << "CPU time used no mp: "
-            << 1000.0 * (c_end1-c_start1) / CLOCKS_PER_SEC << " ms\n"
-            << "Wall clock time passed no mp: "
-            << std::chrono::duration<double, std::milli>(t_end1-t_start1).count()
-            << " ms\n";
+        Results << std::fixed << std::setprecision(2)
+                << num_threads << ", "
+                << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ," 
+                << std::chrono::duration<double, std::milli>(t_end-t_start).count() <<"\n";
     }
     Results.close();
     return 0;
